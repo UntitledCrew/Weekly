9a0c3f61e662a950e70c45ee7a1c7fde04d9cbd3
hopehappy92 <hopehappy92@naver.com>
2021-01-24 21:42:46 +0900

    2021-01-4th os

--- 2021_year/1_month/3_week/jihun/algo/15663.js
+++ 2021_year/1_month/3_week/jihun/algo/15663.js
@@ -38,30 +38,60 @@ const secondLine = input[1].split(' ').map(el => (+el))
 
 
 // 아래는 런타임
-const combination = (lst, depth) => {
-    if (depth === 1) {
-        return lst.map(el => ([el]))
+// const combination = (lst, depth) => {
+//     if (depth === 1) {
+//         return lst.map(el => ([el]))
+//     }
+//     const answer = new Set()
+//     lst.forEach((start, index, origin) => {
+//         const rest = origin.slice(0,index).concat(origin.slice(index+1,))
+//         const recursiveCombination = combination(rest, depth-1)
+//         // 왜 recursive가 set으로 바뀌지?
+//         const temp = recursiveCombination.map(el => [start, ...el])
+//         temp.forEach(el => {
+//             answer.add(el.join(' '))
+//         })
+//     })
+//     return answer
+// }
+
+// const N = firstLine[0]
+// const M = firstLine[1]
+// const nums = secondLine.sort((a, b) => (a - b))
+
+// let answer = ''
+// combination(nums, M).forEach(comb => {
+//     answer = answer + comb + '\n'
+// })
+
+// console.log(answer.trim())
+
+
+// set 사용 안하고
+const combination = (depth) => {
+    if (depth === M) {
+        answerString = answerString + answer.join(' ') + '\n'
+        return
     }
-    const answer = new Set()
-    lst.forEach((start, index, origin) => {
-        const rest = origin.slice(0,index).concat(origin.slice(index+1,))
-        const recursiveCombination = combination(rest, depth-1)
-        // 왜 recursive가 set으로 바뀌지?
-        const temp = recursiveCombination.map(el => [start, ...el])
-        temp.forEach(el => {
-            answer.add(el.join(' '))
-        })
-    })
-    return answer
+    for (let i = 0; i < keys.length; i++) {
+        if (!count[i]) { continue }
+        count[i]--
+        answer[depth] = keys[i]
+        combination(depth+1)
+        count[i]++
+    }
 }
 
 const N = firstLine[0]
 const M = firstLine[1]
-const nums = secondLine.sort((a, b) => (a - b))
+const nums = new Object()
+secondLine.forEach(num => (nums[num] ? nums[num]++ : (nums[num] = 1)))
 
-let answer = ''
-combination(nums, M).forEach(comb => {
-    answer = answer + comb + '\n'
-})
+const keys = Object.keys(nums).map(el => (+el))
+const count = Object.keys(nums).map(el => (nums[el]))
 
-console.log(answer.trim())
+const answer = new Array(M).fill(false)
+let answerString = ''
+combination(0)
+
+console.log(answerString)
